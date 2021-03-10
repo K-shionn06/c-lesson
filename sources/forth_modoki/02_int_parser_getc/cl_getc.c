db8b4948f05d6ba300982acf79a3ecb7682b2408
K-shionn06 <kamishiro_shionn06@protonmail.com>
2021-03-10 15:51:45 +0900

    Add the cl_getc_set_src and _my_strcpy.

--- sources/forth_modoki/02_int_parser_getc/cl_getc.c
+++ sources/forth_modoki/02_int_parser_getc/cl_getc.c
@@ -1,7 +1,9 @@
 #include "clesson.h"
 #include <string.h>
 
-static const char* const input = "123 456";
+#define MAX_SIZE_INPUT 63
+
+static char input[MAX_SIZE_INPUT + 1] = "123 456";
 static int pos = 0;
 
 
@@ -9,4 +11,27 @@ int cl_getc() {
     if(strlen(input) == pos)
         return EOF;
     return input[pos++];
+}
+
+int _my_strcpy(char * dest, char const * src) {
+    int i;
+    
+    if (MAX_SIZE_INPUT < strlen(src))
+        puts("The string has been force truncated in _my_strcpy!");
+
+    for (i = 0; i < MAX_SIZE_INPUT; i++) {
+        *dest++ = *src++;
+        if ('\0' == *src)
+            return i + 1;
+    }
+    dest[MAX_SIZE_INPUT + 1] = '\0';
+
+    return i + 1;
+}
+
+void cl_getc_set_src(char *str) {
+    int test;
+    test = _my_strcpy(input, str);
+    printf("%d\n", test);
+    pos = 0;
 }
