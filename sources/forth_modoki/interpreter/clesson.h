94eadc5a2f502ade2c1a45787137a822f3acc444
K-shionn06 <kamishiro_shionn06@protonmail.com>
2021-04-04 20:29:48 +0900

    Added an include guard to clesson.h

--- sources/forth_modoki/interpreter/clesson.h
+++ sources/forth_modoki/interpreter/clesson.h
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+#ifndef CLESSON_H
+#define CLESSON_H
+
 /*
 return one character and move cursor.
 return EOF if end of file.
 */
 int cl_getc();
 void cl_getc_set_src(char* str);
+
+#endif
