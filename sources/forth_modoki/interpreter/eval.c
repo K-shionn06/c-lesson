beee93b46c020d0bab137a1ef7bd4b41bdbab82a
K-shionn06 <kamishiro_shionn06@protonmail.com>
2021-04-05 12:50:49 +0900

    Enabled to execute exec_array

--- sources/forth_modoki/interpreter/eval.c
+++ sources/forth_modoki/interpreter/eval.c
@@ -41,7 +41,51 @@ static void register_primitives() {
     dict_put_cfunc("def", execute_def);
 }
 
-void eval() {
+static void eval_exec_array(struct EA_ElementArray *byte_codes) {
+    struct EA_Element* byte_code;
+    struct D_Element* d_elem;
+
+    for (int i = 0; i < byte_codes->len; ++i) {
+        byte_code = &byte_codes->elements[i];
+
+        switch (byte_code->dtype) {
+            default:
+                assert(false);
+
+            case EA_NUMBER:
+                stack_push_number(byte_code->u.number);
+                break;
+
+            case EA_LIT_NAME:
+                stack_push_lit_name(byte_code->u.name);
+                break;
+
+            case EA_EXE_NAME:
+                assert( dict_key_isused(byte_code->u.name) );
+
+                d_elem = dict_get(byte_code->u.name);
+                switch (d_elem->dtype) {
+                    default:
+                        assert(false);
+
+                    case D_NUMBER:
+                        stack_push_number(d_elem->u.number);
+                        break;
+
+                    case D_C_FUNC:
+                        d_elem->u.cfunc();
+                        break;
+
+                    case D_BYTE_CODES:
+                        eval_exec_array(d_elem->u.byte_codes);
+                        break;
+                }
+                break;
+        }
+    }
+}
+
+static void eval() {
     int ch = EOF;
     struct Token token = {
         UNKNOWN,
@@ -52,48 +96,56 @@ void eval() {
 
     do {
         ch = parse_one(ch, &token);
-        if (UNKNOWN!= token.ltype) {
-            switch (token.ltype) {
-                default:
-                    assert(false);
+        assert(UNKNOWN != token.ltype);
 
-                case SPACE:
-                    break;
+        switch (token.ltype) {
+            default:
+                assert(false);
 
-                case NUMBER:
-                    stack_push_number(token.u.number);
-                    break;
+            case SPACE:
+                break;
 
-                case EXECUTABLE_NAME:
-                    if ( !dict_key_isused(token.u.name) )
+            case NUMBER:
+                stack_push_number(token.u.number);
+                break;
+
+            case LITERAL_NAME:
+                stack_push_lit_name(token.u.name);
+                break;
+
+            case OPEN_CURLY:
+                ch = compile_exec_array(ch, &ea_elem);
+                stack_push_byte_codes(ea_elem.u.byte_codes);
+                break;
+
+            case EXECUTABLE_NAME:
+                assert( dict_key_isused(token.u.name) );
+
+                d_elem = dict_get(token.u.name);
+                switch (d_elem->dtype) {
+                    default:
                         assert(false);
 
-                    d_elem = dict_get(token.u.name);
-                    switch (d_elem->dtype) {
-                        default:
-                            assert(false);
+                    case D_NUMBER:
+                        stack_push_number(d_elem->u.number);
+                        break;
 
-                        case D_NUMBER:
-                            stack_push_number(d_elem->u.number);
-                            break;
+                    case D_C_FUNC:
+                        d_elem->u.cfunc();
+                        break;
 
-                        case D_C_FUNC:
-                            d_elem->u.cfunc();
-                            break;
-                    }
-                    break;
-
-                case LITERAL_NAME:
-                    stack_push_lit_name(token.u.name);
-                    break;
-
-                case OPEN_CURLY:
-                    compile_exec_array(&ea_elem);
-                    stack_push_byte_codes(ea_elem.u.byte_codes);
-            }
+                    case D_BYTE_CODES:
+                        eval_exec_array(d_elem->u.byte_codes);
+                        break;
+                }
+                break;
         }
     } while (EOF != ch);
 }
+
+
+/* Unit tests */
+
 
 static void test_eval_num_one() {
     char *input = "123";
@@ -165,13 +217,47 @@ static void test_eval_def() {
     assert(expect == actual);
 }
 
+static void test_eval_exec_array() {
+    char *input = "/abc {1 2} def abc add";
+    int expect = 3;
+
+    cl_getc_set_src(input);
+
+    eval();
+
+    int actual = stack_pop_number();
+
+    assert(expect == actual);
+}
+
+static void test_eval_exec_array_complex() {
+    char* input = 
+        "/ZZ {6} def \
+         /YY {ZZ 5 add} def \
+         /ZZYYadd {ZZ YY add} def \
+         ZZYYadd";
+    int expect = 17;
+
+    cl_getc_set_src(input);
+
+    eval();
+
+    int actual = stack_pop_number();
+
+    assert(expect == actual);
+}
+
 int main() {
     register_primitives();
+
     test_eval_num_one();
     test_eval_num_two();
     test_eval_num_add();
     test_eval_lit();
     test_eval_def();
+
+    test_eval_exec_array();
+    test_eval_exec_array_complex();
 
     return 0;
 }
