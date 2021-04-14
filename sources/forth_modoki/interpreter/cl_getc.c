cdecba65f490ad98c1e494bea935177a0565fbb1
K-shionn06 <kamishiro_shionn06@protonmail.com>
2021-04-14 16:09:04 +0900

    cl_getc support file reading

--- sources/forth_modoki/interpreter/cl_getc.c
+++ sources/forth_modoki/interpreter/cl_getc.c
@@ -2,10 +2,15 @@
 #include <string.h>
 
 static const char* input = "123 456";
+static FILE* fp = NULL;
 static int pos = 0;
 
 
 int cl_getc() {
+    if (NULL != fp) {
+        return fgetc(fp);
+    }
+
     if(strlen(input) == pos)
         return EOF;
     return input[pos++];
@@ -14,4 +19,13 @@ int cl_getc() {
 void cl_getc_set_src(char* str){
     input = str;
     pos = 0;
+}
+
+void cl_getc_attach_file(char* file_name) {
+    fp = fopen(file_name, "r");
+}
+
+void cl_getc_detach_file() {
+    fclose(fp);
+    fp = NULL;
 }
--- sources/forth_modoki/interpreter/clesson.h
+++ sources/forth_modoki/interpreter/clesson.h
@@ -6,8 +6,13 @@
 /*
 return one character and move cursor.
 return EOF if end of file.
+
+if fp is not NULL, cl_getc returns a char from the file.
 */
 int cl_getc();
 void cl_getc_set_src(char* str);
+
+void cl_getc_attach_file(char* file_name);
+void cl_getc_detach_file();
 
 #endif
