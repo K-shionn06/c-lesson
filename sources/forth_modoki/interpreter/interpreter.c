ecf4ee7e9184f0f45a23c202d2a2e564b0c166cd
K-shionn06 <kamishiro_shionn06@protonmail.com>
2021-04-15 15:09:56 +0900

    Created tests for sam_k, sam_k_2 and repeat

--- sources/forth_modoki/interpreter/interpreter.c
+++ sources/forth_modoki/interpreter/interpreter.c
@@ -2,18 +2,80 @@
 #include "stack.h"
 #include "eval.h"
 #include <stdio.h>
+#include <assert.h>
 
-void init_interpreter() {
+
+static void init_interpreter() {
     register_primitives();
     stack_clear();
 }
 
-int main() {
-    init_interpreter();
-    cl_getc_attach_file("ps_prog/sample.ps");
 
+/* unit tests */
+
+
+static void eval_with_file(char* file_name) {
+    cl_getc_attach_file(file_name);
+    eval();
+    cl_getc_detach_file();
+}
+
+static void test_ps_repeat() {
+    char* input = "100 4 {10 add} repeat";
+    int expect = 140;
+
+    init_interpreter();
+
+    eval_with_file("ps_prog/repeat.ps");
+
+    cl_getc_set_src(input);
     eval();
 
-    printf("Stack top: %d\n", stack_pop_number());
+    int actual = stack_pop_number();
+
+    assert(expect == actual);
+}
+
+static void test_ps_sam_k() {
+    char* input = "10 sum_k";
+    int expect = 55;
+
+    init_interpreter();
+
+    eval_with_file("ps_prog/sum_k.ps");
+
+    cl_getc_set_src(input);
+    eval();
+
+    int actual = stack_pop_number();
+
+    assert(expect == actual);
+}
+
+static void test_ps_sam_k_2() {
+    char* input = "4 sum_k_2";
+    int expect = 30;
+
+    init_interpreter();
+
+    eval_with_file("ps_prog/sum_k_2.ps");
+
+    cl_getc_set_src(input);
+    eval();
+
+    int actual = stack_pop_number();
+
+    assert(expect == actual);
+}
+
+static void test_suite() {
+    test_ps_repeat();
+    test_ps_sam_k();
+    test_ps_sam_k_2();
+}
+
+int main() {
+    test_suite();
+
     return 0;
 }
